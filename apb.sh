1a58040ea8d11617f037efef4e968013eee30407
Chuck Hinson <190260+chuckhinson@users.noreply.github.com>
2023-02-14 14:44:51 -0500

    Refactor apb.sh to get ready for next steps

--- apb.sh
+++ apb.sh
@@ -2,40 +2,65 @@
 
 set -euo pipefail
 
-INVENTORY_DIR="$(pwd)/tmp/inventory"
-INVENTORY_FILE="$(pwd)/inventory.ini"
-KUBESPRAY_DIR="$(pwd)/kubespray"
-ANSIBLE_CFG="${KUBESPRAY_DIR}/ansible.cfg"
-SSH_CONFIG_FILE="$(pwd)/ssh-config"
-SSH_KEY_FILE="${HOME}/.ssh/k8splay_rsa"
+declare INVENTORY_DIR="$(pwd)/tmp/inventory"
+declare INVENTORY_FILE="$(pwd)/inventory.ini"
+declare KUBESPRAY_DIR="$(pwd)/kubespray"
+declare ANSIBLE_CFG="${KUBESPRAY_DIR}/ansible.cfg"
+declare SSH_CONFIG_FILE="$(pwd)/ssh-config"
+declare SSH_KEY_FILE="${HOME}/.ssh/k8schuck_rsa"
 
-ELB_NAME=$(terraform -chdir=./terraform output -json | jq -r '.elb_dns_name.value')
-sed -i "/^apiserver_loadbalancer_domain_name=/c\apiserver_loadbalancer_domain_name=\"$ELB_NAME\"" "${INVENTORY_FILE}"
+declare ELB_NAME
+declare BASTION_IP
 
-BASTION_IP=$(terraform -chdir=./terraform output -json | jq -r '.jumpbox_public_ip.value')
-sudo sed -i "s/ProxyJump ubuntu.*/ProxyJump ubuntu@${BASTION_IP}/" "${SSH_CONFIG_FILE}"
-sudo chown 0:0 "${SSH_CONFIG_FILE}"
-sudo chmod og-w "${SSH_CONFIG_FILE}"
+function gatherClusterInfoFromTerraform () {
+  ELB_NAME=$(terraform -chdir=./terraform output -json | jq -r '.elb_dns_name.value')
+  BASTION_IP=$(terraform -chdir=./terraform output -json | jq -r '.jumpbox_public_ip.value')
+}
 
-echo "Once connected to the container, run the following commands"
-echo "ssh 10.2.2.10   #Ctl-C once you've accepted the host key"
-echo "apt update && apt install python3-jmespath"
-echo "ansible-playbook -b -i /inventory/inventory.ini cluster.yml"
+function prepareAnsibleInventoryFile () {
+  sed -i "/^apiserver_loadbalancer_domain_name=/c\apiserver_loadbalancer_domain_name=\"$ELB_NAME\"" "${INVENTORY_FILE}"
+}
 
-docker run --rm -it \
-  --mount type=bind,source="${INVENTORY_DIR}",dst=/inventory \
-  --mount type=bind,source="${INVENTORY_FILE}",dst=/inventory/inventory.ini \
-  --mount type=bind,source="${ANSIBLE_CFG}",dst=/kubespray/ansible.cfg \
-  --mount type=bind,source="${SSH_KEY_FILE}",dst=/root/.ssh/id_rsa \
-  --mount type=bind,source="${SSH_CONFIG_FILE}",dst=/root/.ssh/config \
-  quay.io/kubespray/kubespray:v2.21.0 bash
+function setupBastionSshConfig () {
+  sudo sed -i "s/ProxyJump ubuntu.*/ProxyJump ubuntu@${BASTION_IP}/" "${SSH_CONFIG_FILE}"
+  sudo chown 0:0 "${SSH_CONFIG_FILE}"
+  sudo chmod og-w "${SSH_CONFIG_FILE}"
+}
 
+function runAnsibleContainer () {
+  echo "Once connected to the container, run the following commands"
+  echo "ssh 10.2.2.10   #Ctl-C once you've accepted the host key"
+  echo "apt update && apt install python3-jmespath"
+  echo "ansible-playbook -b -i /inventory/inventory.ini cluster.yml"
 
-SRC_ADMIN_CONF=$INVENTORY_DIR/artifacts/admin.conf
-ADMIN_CONF="$(pwd)/tmp/admin.conf"
-if sudo [ -f "$SRC_ADMIN_CONF" ] ; then 
-  sudo cp "$SRC_ADMIN_CONF" "$ADMIN_CONF"
-  sudo chown "$(id -u):$(id -g)" "${ADMIN_CONF}"
-  sed -i "s_server: https://.*_server: https://$ELB_NAME:6443_" "${ADMIN_CONF}"
-  echo "For kubectl access, 'export KUBECONFIG=${ADMIN_CONF}'"
-fi
+  docker run --rm -it \
+    --mount type=bind,source="${INVENTORY_DIR}",dst=/inventory \
+    --mount type=bind,source="${INVENTORY_FILE}",dst=/inventory/inventory.ini \
+    --mount type=bind,source="${ANSIBLE_CFG}",dst=/kubespray/ansible.cfg \
+    --mount type=bind,source="${SSH_KEY_FILE}",dst=/root/.ssh/id_rsa \
+    --mount type=bind,source="${SSH_CONFIG_FILE}",dst=/root/.ssh/config \
+    quay.io/kubespray/kubespray:v2.21.0 bash
+}
+
+function setupKubeAdminConf () {
+  SRC_ADMIN_CONF=$INVENTORY_DIR/artifacts/admin.conf
+  ADMIN_CONF="$(pwd)/tmp/admin.conf"
+  if sudo [ -f "$SRC_ADMIN_CONF" ] ; then
+    sudo cp "$SRC_ADMIN_CONF" "$ADMIN_CONF"
+    sudo chown "$(id -u):$(id -g)" "${ADMIN_CONF}"
+    sed -i "s_server: https://.*_server: https://$ELB_NAME:6443_" "${ADMIN_CONF}"
+    echo "For kubectl access, 'export KUBECONFIG=${ADMIN_CONF}'"
+  fi
+}
+
+function main() {
+
+  gatherClusterInfoFromTerraform
+  prepareAnsibleInventoryFile
+  setupBastionSshConfig
+  runAnsibleContainer
+  setupKubeAdminConf
+
+}
+
+main "$@"
